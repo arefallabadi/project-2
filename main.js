52714d71588278c4757c47ab3d6f4b96d270803b
arefallabadi <labadi.aref@gmail.com>
2021-04-29 16:14:11 +0300

    create function to hide some things in body

---
+++ main.js
@@ -0,0 +1,30 @@
+let user = []
+let massage =[]
+
+const login = $("#login")
+const register = $("#register")
+const contact = $("#contact")
+const about = $("#about")
+const market = $("#market")
+const clothes =$("#clothes")
+const electronic =$("#electronic")
+const kitchen =$("#kitchen")
+const shoes = $("#shoes")
+const bag = $("#bag")
+const buy =$("#buy")
+
+const load = ()=>{
+    register.hide()
+    contact.hide()
+    about.hide()
+     market.hide()
+    clothes.hide()
+    electronic.hide()
+    kitchen.hide()
+    shoes.hide()
+    bag.hide()
+    buy.hide()
+}
+
+
+
